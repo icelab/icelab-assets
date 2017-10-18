9f31f7678ee5a6655ab9d8a978f8e61879f4c571
Max Wheeler <max@makenosound.com>
2017-10-18 15:55:01 +1100

    Use css not postcss as parser value

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -248,7 +248,7 @@ module.exports = {
     // Format CSS with Prettier. JS is handled through the eslint-loader
     // (so that we can mix in other eslint configuration)
     new PrettierPlugin({
-      parser: "postcss",
+      parser: "css",
       extensions: [".css"]
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
